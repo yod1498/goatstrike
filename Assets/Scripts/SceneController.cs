0c295c84b7c40e450ae004f1a997f410077de319
yod <yod1498@yahoo.com>
2016-03-20 12:17:19 +0700

    add menu and story scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+   
+
+	public void gotoMenu(){
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void gotoStory(){
+		SceneManager.LoadScene("Story");
+	}
+
+	public void gotoCredit() {
+		SceneManager.LoadScene("Credit");
+	}
+
+	public void gotoExit(){
+		Application.Quit();
+	}
+
+	public void startGame() {
+		SceneManager.LoadScene("Level1");
+	}
+
+    public static void gotoGame(){
+		SceneManager.LoadScene("Level1");
+    }
+}
---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransition : StateMachineBehaviour 
+{
+
+	override public void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
+		SceneController.gotoGame ();
+	}
+
+}
